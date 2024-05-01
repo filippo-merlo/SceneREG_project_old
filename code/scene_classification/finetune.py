e6448cdc2d1b145eb7e64cb04c84ef5bf89038fa
Filippo <filippo99merlo@gmail.com>
2024-05-01 12:26:12 +0200

    Refactor transform function and update ViTImageProcessor initialization in finetune.py for scene classification

--- code/scene_classification/finetune.py
+++ code/scene_classification/finetune.py
@@ -28,14 +28,10 @@ import torch
 model_name_or_path = 'google/vit-base-patch16-224-in21k'
 processor = ViTImageProcessor.from_pretrained(model_name_or_path)
 
-from transformers import ViTFeatureExtractor
-
-feature_extractor = ViTFeatureExtractor.from_pretrained(model_name_or_path)
-mean = feature_extractor.image_mean
 #%%
 def transform(example_batch):
     # Take a list of PIL images and turn them to pixel values
-    inputs = mean([x for x in example_batch['image']], return_tensors='pt')
+    inputs = processor([x for x in example_batch['image']], return_tensors='pt')
 
     # Don't forget to include the labels!
     inputs['labels'] = example_batch['label']
