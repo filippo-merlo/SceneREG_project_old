2f3c416ac8559e82c54767cafa0f4d6889cf216e
Filippo <filippo99merlo@gmail.com>
2024-05-01 12:35:24 +0200

    Fix typo in 'labels' variable assignment in try_finetune.py for scene classification

--- code/scene_classification/try_finetune.py
+++ code/scene_classification/try_finetune.py
@@ -22,7 +22,7 @@ import torch
 def collate_fn(batch):
     return {
         'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
-        'labels': torch.tensor([x['label'] for x in batch])
+        'labels': torch.tensor([x['labels'] for x in batch])
     }
 
 import numpy as np
