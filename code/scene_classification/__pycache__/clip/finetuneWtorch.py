f650c4238955912c6d5f51aa7098e15c48716926
Filippo <filippo99merlo@gmail.com>
2024-05-14 13:58:17 +0200

    Refactor finetuneWtorch.py for scene classification and import CollectionsDataset

--- code/scene_classification/__pycache__/clip/finetuneWtorch.py
+++ code/scene_classification/__pycache__/clip/finetuneWtorch.py
@@ -1,48 +1,59 @@
 #%%
 from nnet import *
+from dataset import CollectionsDataset
 from dataset import *
 
 from torch.utils.data import DataLoader
 
-
+# Initialize DataLoader
 train_dataloader = DataLoader(CollectionsDataset(final_dataset['train'], processor), shuffle=True, batch_size=8)
-eval_dataloader = DataLoader(CollectionsDataset(final_dataset['train'], processor), shuffle=True, batch_size=8)
+eval_dataloader = DataLoader(CollectionsDataset(final_dataset['test'], processor), shuffle=True, batch_size=8)
 
+# Initialize model
+n_labels = len(final_dataset['train'].features['scene_category'].names)
+model = ClipModelWithClassifier(n_labels)
 
-model = ClipModelWithClassifier()
-
-
+# # Create an optimizer and learning rate scheduler to fine-tune the model. Let's use the AdamW optimizer from PyTorch:
 from torch.optim import AdamW
-
 optimizer = AdamW(model.parameters(), lr=5e-5)
 
-
+#Create the default learning rate scheduler from Trainer:
 from transformers import get_scheduler
-
-num_epochs = 3
+num_epochs = 4
 num_training_steps = num_epochs * len(train_dataloader)
 lr_scheduler = get_scheduler(
     name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
 )
 
-
+# specify device to use a GPU if you have access to one. Otherwise, training on a CPU may take several hours instead of a couple of minutes.
 import torch
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+if torch.backends.mps.is_available():
+   device = torch.device("mps")
+   print('mps Ok')
+elif torch.cuda.is_available():
+    device = torch.device('cuda')
+    print('cuda Ok')
+else:
+   device = torch.device('cpu')
+   print ("cpu OK")
+
 model.to(device)
 
+# To keep track of your training progress, use the tqdm library to add a progress bar over the number of training steps:
 from tqdm.auto import tqdm
+import torch.nn.functional as F
 
 progress_bar = tqdm(range(num_training_steps))
 
 model.train()
 for epoch in range(num_epochs):
     for batch in train_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(**batch)
-        loss = outputs.loss
+        actual = batch['labels'].to(device)
+        input = {k:v.squeeze().to(device) for k, v in batch['image'].items()}
+        outputs = model(input)
+        loss = F.cross_entropy(outputs, torch.argmax(actual, dim=1))
         loss.backward()
-
         optimizer.step()
         lr_scheduler.step()
         optimizer.zero_grad()
