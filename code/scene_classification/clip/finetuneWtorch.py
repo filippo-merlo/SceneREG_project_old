46f70b9f91a3d49d8ebd43a278d5a2f3c472e59f
Filippo <filippo99merlo@gmail.com>
2024-05-14 16:59:44 +0200

    Refactor finetuneWtorch.py for scene classification and import wandb for logging

--- code/scene_classification/clip/finetuneWtorch.py
+++ code/scene_classification/clip/finetuneWtorch.py
@@ -1,30 +1,41 @@
+#%%
 from nnet import *
-from dataset import CollectionsDataset
+from dataset import CollectionsDataset, final_dataset, processor
 from dataset import *
 from config import *
 from torch.utils.data import DataLoader
 # wandb
 import wandb
-wandb.login()
+#wandb.login()
+config = {
+    "model_checkpoint": model_checkpoint,
+    "batch_size": 16,
+    "num_epochs": 4,
+    "lr": 2e-5,
+    "momentum": 0.9
+}
+
+wandb.init(project=model_checkpoint.split('/')[1], config=config)
 
 # Initialize DataLoader
-train_dataloader = DataLoader(CollectionsDataset(final_dataset['train'], processor), shuffle=True, batch_size=8)
-eval_dataloader = DataLoader(CollectionsDataset(final_dataset['test'], processor), shuffle=True, batch_size=8)
+train_dataloader = DataLoader(CollectionsDataset(final_dataset['train'], processor), shuffle=True, batch_size=wandb.config['batch_size'])
+eval_dataloader = DataLoader(CollectionsDataset(final_dataset['test'], processor), shuffle=True, batch_size=wandb.config['batch_size'])
 
 # Initialize model
 n_labels = len(final_dataset['train'].features['scene_category'].names)
 model = ClipModelWithClassifier(n_labels)
 
 # # Create an optimizer and learning rate scheduler to fine-tune the model. Let's use the AdamW optimizer from PyTorch:
-from torch.optim import AdamW
-optimizer = AdamW(model.parameters(), lr=5e-5)
+from torch.optim import sgd
+
+optimizer = sgd(model.parameters(), lr=wandb.config['lr'], momentum=wandb.config['momentum'])
 
 #Create the default learning rate scheduler from Trainer:
 from transformers import get_scheduler
-num_epochs = 4
+num_epochs = wandb.config['num_epochs']
 num_training_steps = num_epochs * len(train_dataloader)
 lr_scheduler = get_scheduler(
-    name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+    name="linear", optimizer=optimizer, num_warmup_steps=100, num_training_steps=num_training_steps
 )
 
 # specify device to use a GPU if you have access to one. Otherwise, training on a CPU may take several hours instead of a couple of minutes.
@@ -43,7 +54,6 @@ else:
 model.to(device)
 
 # Magic
-wandb.init(project=model_checkpoint.split('/')[1])
 log_freq = 100
 wandb.watch(model, log_freq=log_freq)
 
@@ -53,8 +63,13 @@ import torch.nn.functional as F
 
 progress_bar = tqdm(range(num_training_steps))
 
-model.train()
+import evaluate
+
+metric = evaluate.load("accuracy", cache_dir=cache_dir)
+
+
 for epoch in range(num_epochs):
+    model.train()
     for batch_idx, batch in enumerate(train_dataloader):
         actual = batch['labels'].to(device)
         input = {k:v.squeeze().to(device) for k, v in batch['image'].items()}
@@ -68,18 +83,13 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         progress_bar.update(1)
 
-
-import evaluate
-
-metric = evaluate.load("accuracy", cache_dir=cache_dir)
-model.eval()
-for batch in eval_dataloader:
-    batch = {k: v.to(device) for k, v in batch.items()}
-    with torch.no_grad():
-        outputs = model(**batch)
-
-    logits = outputs.logits
-    predictions = torch.argmax(logits, dim=-1)
-    metric.add_batch(predictions=predictions, references=batch["labels"])
-
-metric.compute()
+    model.eval()
+    for batch in eval_dataloader:
+        actual = batch['labels'].to(device)
+        input = {k:v.squeeze().to(device) for k, v in batch['image'].items()}
+        with torch.no_grad():
+            outputs = model(input)
+        predictions = torch.argmax(outputs, dim=-1)
+        metric.add_batch(predictions=predictions, references=actual)
+    metric = metric.compute()
+    wandb.log({'acc' : metric})
--- code/scene_classification/clip/nnet.py
+++ code/scene_classification/clip/nnet.py
@@ -2,16 +2,22 @@
 from transformers import CLIPVisionModel
 import torch
 from config import * 
+import torch.nn.functional as F
 class ClipModelWithClassifier(torch.nn.Module):
     def __init__(self, num_labels):
-        super(ClipModelWithClassifier, self).__init__()
+        super().__init__()
         self.clip_model = CLIPVisionModel.from_pretrained(model_checkpoint, cache_dir=cache_dir)
-        self.classifier_head = torch.nn.Linear(in_features=768, out_features=num_labels)
+        self.fc1 = torch.nn.Linear(in_features=768, out_features=600)
+        self.fc2 = torch.nn.Linear(in_features=600, out_features=500)
+        self.classifier_head = torch.nn.Linear(in_features=500, out_features=num_labels)
 
     def forward(self, input):
         outputs = self.clip_model(**input)
-        #last_hidden_state = outputs.last_hidden_state
-        pooled_output = outputs.pooler_output  # pooled CLS states
-        logits = self.classifier_head(pooled_output)
+        #pooled_output = outputs.pooler_output  # pooled CLS states
+        last_hidden_state = outputs.last_hidden_state
+        pooled_last_hidden_state = last_hidden_state.mean(dim=1) 
+        x = F.relu(self.fc1(pooled_last_hidden_state))
+        x = F.relu(self.fc2(x))
+        logits = self.classifier_head(x)
         probabilities = torch.softmax(logits, dim=-1)
         return probabilities
