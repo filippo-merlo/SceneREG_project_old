dbb199285de4bfaf188fd57706c11595c421e731
Filippo <filippo99merlo@gmail.com>
2024-05-16 19:02:30 +0200

    Refactor dataset_prep.py for efficient scene classification and update variable names

--- code/scene_classification/finetuning/dataset_prep.py
+++ code/scene_classification/finetuning/dataset_prep.py
@@ -95,7 +95,7 @@ with open('new_labels.json', 'w') as f:
 '''
 import json
 import os 
-with open(os.path.join(os.getcwd(),'new_labels.json'), 'r') as f:
+with open('/home/filippo.merlo/SceneREG_project/code/scene_classification/finetuning/new_labels.json'), 'r') as f:
     new_labels = json.load(f)
 print(new_labels['scene_ids'])
 new_scene_categories = [c for i, c in enumerate(new_labels['scene_labels']) if i in new_labels['scene_ids']]
