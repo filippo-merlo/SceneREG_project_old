d69ff99756c5b5b64b138cbae4880611ba7c7efb
Filippo <filippo99merlo@gmail.com>
2024-05-20 13:12:30 +0200

    Refactor dataset_prep.py and finetune.py for scene classification

--- code/scene_classification/finetuning/hf_vit/finetune.py
+++ code/scene_classification/finetuning/hf_vit/finetune.py
@@ -75,7 +75,7 @@ label_len = len(new_names2id.keys())
 print(label_len)
 print(len(datasets_processed['train'].features['scene_category'].names))
 print(len(datasets_processed['test'].features['scene_category'].names))
-print(datasets_processed['train']['scene_category'])
+print(datasets_processed['train']['labels'])
 
 def model_init():
     vit_model = ViTForImageClassification.from_pretrained(
