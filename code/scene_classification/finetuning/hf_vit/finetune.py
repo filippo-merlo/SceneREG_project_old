f296bc04521759f1874b4618619b25ee86326064
Filippo <filippo99merlo@gmail.com>
2024-05-20 13:31:09 +0200

    Refactor dataset_prep.py and finetune.py for scene classification

--- code/scene_classification/finetuning/hf_vit/finetune.py
+++ code/scene_classification/finetuning/hf_vit/finetune.py
@@ -31,8 +31,9 @@ def transform(example_batch):
     inputs['labels'] = example_batch['scene_category']
     return inputs
 
+print(len(final_dataset['train']['scene_category']))
 datasets_processed = final_dataset.with_transform(transform)
-
+print(len(datasets_processed['train']['scene_category']))
 import torch
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
    
