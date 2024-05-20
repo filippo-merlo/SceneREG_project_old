636e37d9d8de7363b79cedff39a3d7d8278d4e5f
Filippo <filippo99merlo@gmail.com>
2024-05-20 20:23:12 +0200

    Refactor YOLO model training code to use specified data path, batch size, and save directory

--- code/scene_classification/finetuning/yolo/finetune_yolo.py
+++ code/scene_classification/finetuning/yolo/finetune_yolo.py
@@ -1,11 +1,34 @@
 # import YOLO model
 from ultralytics import YOLO
+import os
 
 # Load a model
 model = YOLO('yolov8n-cls.pt', ) # load a pretrained model (recommended for training)
 
-# Train the model
-model.train(data='/kaggle/working/data', epochs=5)
+data_path = '/mnt/cimec-storage6/users/filippo.merlo/ADE20K_2016_07_26'
+n_epochs = 100
+bs = 16
+gpu_id = 0
+verbose = True
+rng = 0
+validate = True
+
+# Specify the save directory for training runs
+save_dir = '/mnt/cimec-storage6/users/filippo.merlo/yolo'
+os.makedirs(save_dir, exist_ok=True)
+
+    # Train the model
+model = YOLO('yolov8n.pt')
+results = model.train(
+    data=data_path,
+    epochs=n_epochs,
+    batch=bs,
+    device=gpu_id,
+    verbose=verbose,
+    seed=rng,
+    val=validate,
+    save_dir=save_dir
+)
 
 # Validate the model
 metrics = model.val() # no arguments needed, dataset and settings remembered
