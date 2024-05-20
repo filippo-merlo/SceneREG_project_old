7dfa1c582b002b6065aa260da3a86aa15cc1d6ee
Filippo <filippo99merlo@gmail.com>
2024-05-20 13:37:24 +0200

    Refactor dataset_prep.py to improve scene classification

--- code/scene_classification/finetuning/dataset_prep.py
+++ code/scene_classification/finetuning/dataset_prep.py
@@ -96,6 +96,10 @@ with open('new_labels.json', 'w') as f:
 import json
 with open('/home/filippo.merlo/SceneREG_project/code/scene_classification/finetuning/hf_vit/new_labels.json', 'r') as f:
     new_labels = json.load(f)
+print(len(new_labels['scene_labels']))
+print(len(new_labels['scene_ids']))
+print(len(new_labels['img_label_ass']))
+
 new_scene_categories = [c for i, c in enumerate(new_labels['scene_labels']) if i in new_labels['scene_ids']]
 new_label_ids = new_labels['img_label_ass']
 
