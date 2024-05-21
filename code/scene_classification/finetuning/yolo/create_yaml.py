677084291126a7c32e620a7b10e696f3ada85201
Filippo <filippo99merlo@gmail.com>
2024-05-21 14:27:48 +0200

    Refactor YOLO model training code to use consistent annotation file creation

--- code/scene_classification/finetuning/yolo/create_yaml.py
+++ code/scene_classification/finetuning/yolo/create_yaml.py
@@ -9,44 +9,28 @@ def ensure_dir_exists(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
-def get_category_mapping(src):
-    # Get all first-level subdirectories in the training set as categories
-    category_mapping = {}
-    training_dir = os.path.join(src, 'training')
-    for letter_dir in os.listdir(training_dir):
-        letter_path = os.path.join(training_dir, letter_dir)
-        if os.path.isdir(letter_path):
-            for category in os.listdir(letter_path):
-                category_path = os.path.join(letter_path, category)
-                if os.path.isdir(category_path):
-                    category_mapping[category] = category
-    return category_mapping
-
-def copy_files(src, dst, category_mapping):
+def copy_files(src, dst):
     for root, _, files in os.walk(src):
         for file in files:
-            if file.endswith('.png'):
-                category = os.path.basename(root)
-                if category in category_mapping:
-                    target_category = category_mapping[category]
-                    target_path = os.path.join(dst, target_category)
-                    ensure_dir_exists(target_path)
-                    shutil.move(os.path.join(root, file), os.path.join(target_path, file))
+            if file.endswith('.jpg'):
+                # Get the category name which is the parent directory of the current directory
+                category = os.path.basename(os.path.dirname(root))
+                target_path = os.path.join(dst, category)
+                ensure_dir_exists(target_path)
+                shutil.move(os.path.join(root, file), os.path.join(target_path, file))
 
 def main():
     # Directories to process
     dirs_to_process = {
         'training': 'train',
-        'validation': 'test'
+        'validation': 'val'
     }
-
-    category_mapping = get_category_mapping(source_dir)
 
     for src_subdir, dst_subdir in dirs_to_process.items():
         src_path = os.path.join(source_dir, src_subdir)
         dst_path = os.path.join(target_dir, dst_subdir)
         ensure_dir_exists(dst_path)
-        copy_files(src_path, dst_path, category_mapping)
+        copy_files(src_path, dst_path)
 
 if __name__ == '__main__':
     main()
