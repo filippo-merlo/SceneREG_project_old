917db8801e1d488dd89769867c75ba277c6b2784
Filippo <filippo99merlo@gmail.com>
2024-05-21 14:01:01 +0200

    Refactor YOLO model training code to use consistent annotation file creation

--- code/scene_classification/finetuning/yolo/create_yaml.py
+++ code/scene_classification/finetuning/yolo/create_yaml.py
@@ -1,74 +1,52 @@
 import os
 import shutil
 
-def create_class_directories(base_dir, class_names):
-    for class_name in class_names:
-        class_dir = os.path.join(base_dir, class_name)
-        if not os.path.exists(class_dir):
-            os.makedirs(class_dir)
+# Define the source and target directories
+source_dir = '/mnt/cimec-storage6/users/filippo.merlo/ADE20K_2016_07_26/images'
+target_dir = '/mnt/cimec-storage6/users/filippo.merlo/ade20k_adapted'
 
-def move_images_and_create_annotations(source_root, target_root, phase):
-    for root, dirs, files in os.walk(os.path.join(source_root, f'images/{phase}')):
-        for class_dir in dirs:
-            class_path = os.path.join(root, class_dir)
-            for subdir, _, img_files in os.walk(class_path):
-                for img_file in img_files:
-                    img_src_path = os.path.join(subdir, img_file)
-                    class_target_dir = os.path.join(target_root, phase, class_dir)
-                    img_dst_path = os.path.join(class_target_dir, img_file)
-                    shutil.move(img_src_path, img_dst_path)
+def ensure_dir_exists(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
-def get_class_names(source_dir):
-    class_names = set()
+def get_category_mapping(src):
+    # Get all first-level subdirectories in the training set as categories
+    category_mapping = {}
+    training_dir = os.path.join(src, 'training')
+    for letter_dir in os.listdir(training_dir):
+        letter_path = os.path.join(training_dir, letter_dir)
+        if os.path.isdir(letter_path):
+            for category in os.listdir(letter_path):
+                category_path = os.path.join(letter_path, category)
+                if os.path.isdir(category_path):
+                    category_mapping[category] = category
+    return category_mapping
 
-    for root, dirs, files in os.walk(os.path.join(source_dir, 'images/training')):
-        for directory in dirs:
-            class_names.add(directory)
+def copy_files(src, dst, category_mapping):
+    for root, _, files in os.walk(src):
+        for file in files:
+            if file.endswith('.png'):
+                category = os.path.basename(root)
+                if category in category_mapping:
+                    target_category = category_mapping[category]
+                    target_path = os.path.join(dst, target_category)
+                    ensure_dir_exists(target_path)
+                    shutil.move(os.path.join(root, file), os.path.join(target_path, file))
 
-    for root, dirs, files in os.walk(os.path.join(source_dir, 'images/validation')):
-        for directory in dirs:
-            class_names.add(directory)
+def main():
+    # Directories to process
+    dirs_to_process = {
+        'training': 'train',
+        'validation': 'test'
+    }
 
-    return class_names
+    category_mapping = get_category_mapping(source_dir)
 
-def transform_dataset(source_dir, target_dir):
-    # Get all class names
-    class_names = get_class_names(source_dir)
+    for src_subdir, dst_subdir in dirs_to_process.items():
+        src_path = os.path.join(source_dir, src_subdir)
+        dst_path = os.path.join(target_dir, dst_subdir)
+        ensure_dir_exists(dst_path)
+        copy_files(src_path, dst_path, category_mapping)
 
-    # Create target directory structure
-    for phase in ['train', 'val']:
-        phase_dir = os.path.join(target_dir, phase)
-        if not os.path.exists(phase_dir):
-            os.makedirs(phase_dir)
-        create_class_directories(phase_dir, class_names)
-
-    # Move images and create annotations
-    for phase in ['train', 'val']:
-        move_images_and_create_annotations(source_dir, target_dir, phase)
-
-
-# Example usage:
-transform_dataset('/mnt/cimec-storage6/users/filippo.merlo/ADE20K_2016_07_26', '/mnt/cimec-storage6/users/filippo.merlo/ade20k_adapted')
-
-#import os
-#
-#def get_class_names(source_dir):
-#    class_names = set()
-#
-#    # Get class names from training data
-#    for root, dirs, files in os.walk(os.path.join(source_dir, 'images/training')):
-#        for directory in dirs:
-#            class_names.add(directory)
-#
-#    # Get class names from validation data
-#    for root, dirs, files in os.walk(os.path.join(source_dir, 'images/validation')):
-#        for directory in dirs:
-#            class_names.add(directory)
-#
-#    return class_names
-#
-## Example usage:
-#source_dir = '/mnt/cimec-storage6/users/filippo.merlo/ADE20K_2016_07_26'
-#class_names = get_class_names(source_dir)
-#for class_name in sorted(class_names):
-#    print(class_name)
+if __name__ == '__main__':
+    main()
