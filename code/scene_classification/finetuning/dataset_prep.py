def75d94443fdab91c8d8023de669ed75fe26184
Filippo <filippo99merlo@gmail.com>
2024-05-26 13:28:40 +0200

    Refactor scene classification mapping logic and update label filtering criteria

--- code/scene_classification/finetuning/dataset_prep.py
+++ code/scene_classification/finetuning/dataset_prep.py
@@ -179,7 +179,7 @@ id2names = dict(zip(range(len(names)), names))
 tot_labs = dataset['scene_category']
 counter = Counter(tot_labs)
 
-print({k: v for k, v in counter.items() if v >= 40})
+print({names[k] for k, v in counter.items() if v >= 40})
 
 # Get the labels
 labels = list(counter.keys())
