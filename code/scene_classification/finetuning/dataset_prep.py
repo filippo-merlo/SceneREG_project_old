e786cd11880cfbecf70e3752173d82d1a6af9f1f
Filippo <filippo99merlo@gmail.com>
2024-05-26 12:30:35 +0200

    Refactor scene classification mapping logic and update label filtering criteria

--- code/scene_classification/finetuning/dataset_prep.py
+++ code/scene_classification/finetuning/dataset_prep.py
@@ -193,29 +193,29 @@ from pprint import pprint
 
 pprint(names2id_filtered.values())
 
-filter_dataset = dataset.filter(lambda example: example['scene_category'] in names2id_filtered.values())
-
-# make dicts
-new_names2id = dict()
-
-for i, name in enumerate(names2id_filtered.keys()):
-    new_names2id[name] = i
-
-# reverse dict
-id2names = {v: k for k, v in new_names2id.items()}
-old_2_new_map = dict()
-
-for name, old_id in names2id_filtered.items():
-    new_id = new_names2id[name]
-    old_2_new_map[old_id] = new_id
-
-### ADJUST LABELS
-
-# map old labels to new labels
-new_labels= [old_2_new_map[x] for x in filter_dataset['scene_category']]
-final_dataset = filter_dataset.remove_columns('scene_category').add_column('scene_category', new_labels)
-
-# Redefine class labels
-class_labels = ClassLabel(names=list(names2id_filtered.keys()), num_classes=len(names2id_filtered.keys()))
-final_dataset =  final_dataset.cast_column('scene_category', class_labels)
-final_dataset = final_dataset.train_test_split(test_size=0.1)
+#filter_dataset = dataset.filter(lambda example: example['scene_category'] in names2id_filtered.values())
+#
+## make dicts
+#new_names2id = dict()
+#
+#for i, name in enumerate(names2id_filtered.keys()):
+#    new_names2id[name] = i
+#
+## reverse dict
+#id2names = {v: k for k, v in new_names2id.items()}
+#old_2_new_map = dict()
+#
+#for name, old_id in names2id_filtered.items():
+#    new_id = new_names2id[name]
+#    old_2_new_map[old_id] = new_id
+#
+#### ADJUST LABELS
+#
+## map old labels to new labels
+#new_labels= [old_2_new_map[x] for x in filter_dataset['scene_category']]
+#final_dataset = filter_dataset.remove_columns('scene_category').add_column('scene_category', new_labels)
+#
+## Redefine class labels
+#class_labels = ClassLabel(names=list(names2id_filtered.keys()), num_classes=len(names2id_filtered.keys()))
+#final_dataset =  final_dataset.cast_column('scene_category', class_labels)
+#final_dataset = final_dataset.train_test_split(test_size=0.1)
