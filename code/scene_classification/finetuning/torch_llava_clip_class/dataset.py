9d29f94a318947679ae8832c20c6ea6f90d75085
Filippo <filippo99merlo@gmail.com>
2024-05-27 15:13:34 +0200

    Refactor dataset.py to use processor for LLAVA caption generation

--- code/scene_classification/finetuning/torch_llava_clip_class/dataset.py
+++ code/scene_classification/finetuning/torch_llava_clip_class/dataset.py
@@ -16,22 +16,19 @@ class CollectionsDataset(Dataset):
         self.pipe = processor['llava_pipeline']
         self.num_classes = len(self.data.features['scene_category'].names)
 
-        #processor.tokenizer.add_tokens(["<image>", "<pad>"], special_tokens=True) 
-        #model.resize_token_embeddings(len(processor.tokenizer))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         image = self.data[idx]['image']
-        print(image)
         label = self.data[idx]['scene_category']
         label_tensor = torch.zeros(self.num_classes)
         label_tensor[label] = 1
 
         if self.processor:
             llava_caption = self.pipe(image, prompt="USER: <image>\nWhere is the picture taken?\nASSISTANT:", generate_kwargs={"max_new_tokens": 200})
-            inputs = self.processor(text=llava_caption, images=image, return_tensors="pt", padding=True).to(device)
+            inputs = self.processor(text=str(llava_caption), images=image, return_tensors="pt", padding=True).to(device)
             outputs = self.clip(**inputs)
             txt_features = outputs.text_model_output
             img_features = outputs.vision_model_output
