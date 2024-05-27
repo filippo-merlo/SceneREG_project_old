22543f0827785db60bded648e8ed60fc850da75a
Filippo <filippo99merlo@gmail.com>
2024-05-27 22:37:33 +0200

    Refactor dataset.py to use clip_processor for LLAVA caption generation

--- code/scene_classification/finetuning/torch_llava_clip_class/dataset.py
+++ code/scene_classification/finetuning/torch_llava_clip_class/dataset.py
@@ -30,8 +30,8 @@ class CollectionsDataset(Dataset):
 
        # process image and text
         llava_inputs = self.llava_processor(self.prompt, image, return_tensors='pt').to(0, torch.float16)
-        llava_caption = self.llava.generate(**llava_inputs, max_new_tokens=150, do_sample=False)
-        inputs = self.clip_processor(text=str(llava_caption), images=image, return_tensors="pt", padding=True).to(device0)
+        llava_caption = self.llava.generate(**llava_inputs, max_new_tokens=200, do_sample=False)
+        inputs = self.clip_processor(text=[str(llava_caption)], images=image, return_tensors="pt", padding=True).to(device0)
         outputs = self.clip(**inputs)
         txt_features = outputs.text_model_output.last_hidden_state.mean(dim=1) 
         img_features = outputs.vision_model_output.last_hidden_state.mean(dim=1) 
