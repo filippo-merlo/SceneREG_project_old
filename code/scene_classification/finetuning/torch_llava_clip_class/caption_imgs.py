6c56aa0f87c19afe93c5fca32b15cce3be9b0051
Filippo <filippo99merlo@gmail.com>
2024-05-28 14:57:05 +0200

    Refactor device assignment in caption_imgs.py for consistency

--- code/scene_classification/finetuning/torch_llava_clip_class/caption_imgs.py
+++ code/scene_classification/finetuning/torch_llava_clip_class/caption_imgs.py
@@ -14,8 +14,8 @@ os.environ['HF_HOME'] = cache_dir
 # Specify Device (GPU/CPU)
 import torch
 
-device0 = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-device1 = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+device0 = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+device1 = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # Initialize DataLoader and Preprocessor
 from transformers import AutoProcessor, CLIPModel, LlavaForConditionalGeneration, BitsAndBytesConfig
