27251dcffc5e633b56e4d62fe0d8fea3f229fa23
Filippo <filippo99merlo@gmail.com>
2024-06-09 15:22:17 +0200

    refactor: Update sun_dataset_prep.py with additional class information

--- code/scene_classification/finetuning/sun_dataset_prep.py
+++ code/scene_classification/finetuning/sun_dataset_prep.py
@@ -5,16 +5,13 @@ import random
 import torchvision
 import torch
 from torch.utils.data import DataLoader
-from transformers import ViTImageProcessor
 
-checkpoint = 'google/vit-base-patch16-224'
-processor = ViTImageProcessor.from_pretrained(checkpoint, cache_dir= cache_dir)
 sun_data = torchvision.datasets.SUN397(root = cache_dir, download = True)
-sun_classes = sun_data.class_to_idx
+sun_classes = list(sun_data.class_to_idx.keys())
 
 from datasets import load_dataset, concatenate_datasets, DatasetDict, ClassLabel
 ade_data = load_dataset("scene_parse_150", cache_dir=cache_dir)
-ade_classes = ade_data['train'].features['scene_category'].names
+ade_classes = list(ade_data['train'].features['scene_category'].names)
 
 from pprint import pprint
 print('SUN classes:', len(sun_classes))
