89b04c3891afc77f3566fc32479471b4cf76b375
Filippo <filippo99merlo@gmail.com>
2024-06-12 13:37:22 +0200

    refactor: Update sun_dataset_prep.py with improved class handling and possible matches

--- code/scene_classification/finetuning/sun_dataset_prep.py
+++ code/scene_classification/finetuning/sun_dataset_prep.py
@@ -21,7 +21,8 @@ print(ade_classes[0:20])
 print('Classes in SUN but not in ADE:')
 not_matched_sun = set(sun_classes)-set(ade_classes)
 not_matched_ade = set(ade_classes)-set(sun_classes)
-
+pprint(not_matched_sun)
+pprint(not_matched_ade)
 possible_matches = {}
 for x in not_matched_sun:
     possible_matches[x] = []
@@ -35,46 +36,50 @@ for x in not_matched_sun:
         for i in l:
             if i in l2:
                 possible_matches[x].append(y)
-pprint(possible_matches)
-'''
-# Ajust manually possible matches
-possible_matches = {'bakery_shop': [] ,
- 'balcony_exterior':  'upper_balcony' ,
- 'canal_natural':  [] ,
- 'canal_urban':  'urban' ,
- 'car_interior_backseat':  'backseat' ,
- 'car_interior_frontseat':  'frontseat' ,
- 'covered_bridge_exterior':  'bridge' ,
- 'cubicle_office':  'office' ,
- 'desert_sand':  'sand' ,
- 'desert_vegetation':  'desert_road' ,
- 'dinette_vehicle':'vehicle' ,
- 'elevator_door': 'elevator_interior' ,
- 'field_cultivated': 'cultivated',
- 'field_wild':'wild' ,
- 'forest_broadleaf': 'broadleaf' ,
- 'forest_needleleaf': 'needleleaf' ,
- 'gazebo_exterior': 'gazebo_interior' ,
- 'lake_natural': ['natural','natural_spring'] ,
- 'moat_water': 'water_gate' ,
- 'poolroom_establishment': 'poolroom_home' ,
- 'skatepark': [] ,
- 'stadium_baseball': ['baseball_field', 'baseball'] ,
- 'stadium_football': ['football', 'football_field'] ,
- 'subway_station_platform': ['subway_interior',
-                             'platform'] ,
- 'temple_east_asia': 'east_asia' ,
- 'temple_south_asia': 'south_asia' ,
- 'theater_indoor_procenium': 'indoor_procenium' ,
- 'theater_indoor_seats': ['movie_theater_indoor',
-                          'indoor_seats'] ,
- 'train_station_platform': ['platform',
-                            'train_station_outdoor',
-                            'station'],
- 'underwater_coral_reef':  'coral_reef' ,
- 'waterfall_block': 'block',
- 'waterfall_fan': 'fan',
- 'waterfall_plunge': 'plunge',
- 'wine_cellar_barrel_storage': 'barrel_storage',
- 'wine_cellar_bottle_storage': 'bottle_storage'}
-'''
+
+
+possible_matches = {'bakery_shop': ['shop'],
+ 'balcony_exterior': ['upper_balcony'],
+ 'canal_natural': ['natural'],
+ 'canal_urban': ['urban'],
+ 'car_interior_backseat': ['backseat'],
+ 'car_interior_frontseat': ['frontseat'],
+ 'covered_bridge_exterior': ['covered_bridge_interior'],
+ 'cubicle_office': ['office_cubicles'],
+ 'desert_sand': ['sand'],
+ 'desert_vegetation': ['desert_road'],
+ 'dinette_vehicle': ['vehicle'],
+ 'elevator_door': ['elevator_lobby'],
+ 'field_cultivated': ['cultivated'],
+ 'field_wild': ['wild'],
+ 'forest_broadleaf': ['broadleaf'],
+ 'forest_needleleaf': ['needleleaf'],
+ 'gazebo_exterior': ['gazebo_interior'],
+ 'lake_natural': ['natural_spring'],
+ 'moat_water': ['water_gate'],
+ 'poolroom_establishment': ['establishment'],
+ 'skatepark': ['roller_skating_outdoor'],
+ 'stadium_baseball': ['baseball'],
+ 'stadium_football': ['football'],
+ 'subway_station_platform': ['station'],
+ 'temple_east_asia': ['east_asia'],
+ 'temple_south_asia': ['south_asia'],
+ 'theater_indoor_procenium': ['indoor_procenium'],
+ 'theater_indoor_seats': ['indoor_seats'],
+ 'train_station_platform': ['station'],
+ 'underwater_coral_reef': ['coral_reef'],
+ 'waterfall_block': ['block'],
+ 'waterfall_fan': ['fan'],
+ 'waterfall_plunge': ['plunge'],
+ 'wine_cellar_barrel_storage': ['barrel_storage'],
+ 'wine_cellar_bottle_storage': ['bottle_storage']}
+
+sun2ade_map = {}
+
+for sun_c in sun_classes:
+    sun_c_r = sun_c.replace('/', '_')
+    if sun_c_r in ade_classes:
+        sun2ade_map[sun_c] = sun_c_r
+    if sun_c_r in possible_matches.keys():
+        sun2ade_map[sun_c] = possible_matches[sun_c_r][0]
+print(len(sun2ade_map))
