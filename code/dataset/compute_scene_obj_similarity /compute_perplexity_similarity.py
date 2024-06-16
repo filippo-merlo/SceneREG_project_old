7bebd3dd95800283d636bad55772778770bc3d71
Filippo <filippo99merlo@gmail.com>
2024-06-16 15:46:47 +0200

    refactor: Update compute_perplexity_similarity.py to use a different range of scene categories and candidates

--- code/dataset/compute_scene_obj_similarity /compute_perplexity_similarity.py
+++ code/dataset/compute_scene_obj_similarity /compute_perplexity_similarity.py
@@ -25,8 +25,7 @@ def generate_ranking(prompt, options, model=None, tokenizer=None, log=False):
         for i in range(target_ids.size(1)):
             # Get the model output (logits) and compute log probabilities
             outputs = model(input_ids=current_input_ids)
-            print('outputs')
-            print(outputs)
+            print(outputs.logiuts.size())
             logprobs = torch.nn.functional.log_softmax(outputs.logits, dim=-1)
             # Store the logits for the current step
             next_target_token_id = target_ids[:, i].item()
@@ -83,3 +82,5 @@ for scene_name in scenes_categories[:1]:
         for i, (option, score) in enumerate(results[:5]):
             print(f"{i+1}. {option}: {score:.2f}")
         print("\n")
+
+
