2148df430efe692e487b013e18917d45fe934161
Filippo <filippo99merlo@gmail.com>
2024-06-17 20:52:59 +0200

    refactor: Update compute_perplexity_similarity.py to iterate over 1000 candidates instead of 300

---
+++ code/dataset/compute_scene_obj_similarity /collect_llms_norms.py
@@ -0,0 +1,67 @@
+#%%
+device = "cuda:1"
+CACHE_DIR = '/mnt/cimec-storage6/shared'
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+ACCESS_TOKEN = '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'
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+dtype = torch.bfloat16
+
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir=CACHE_DIR, token=ACCESS_TOKEN)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=dtype,
+    device_map=device,
+    cache_dir=CACHE_DIR, 
+    token=ACCESS_TOKEN
+)
+
+#%%
+import pickle as pkl
+# get objects and scenes names 
+
+# Load object categories from ADE20K 
+DATASET_PATH = '/mnt/cimec-storage6/users/filippo.merlo/ADE20K_2016_07_26'
+index_file = 'index_ade20k.pkl'
+with open('{}/{}'.format(DATASET_PATH, index_file), 'rb') as f:
+    index_ade20k = pkl.load(f)
+candidates = index_ade20k['objectnames']
+
+def collect_norm(model, tokenizer, prompt):
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+    outputs = model(input_ids=input_ids)
+    return outputs
+
+prompt = 'How likely is in a scale of 1 to 100 that the object: [OBJECT] is present in the scene: [SCENE]?'
+
+
+
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    return_tensors="pt"
+).to(model.device)
+
+terminators = [
+    tokenizer.eos_token_id,
+    tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
+
+outputs = model.generate(
+    input_ids,
+    max_new_tokens=256,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+)
+response = outputs[0][input_ids.shape[-1]:]
+print(tokenizer.decode(response, skip_special_tokens=True))
