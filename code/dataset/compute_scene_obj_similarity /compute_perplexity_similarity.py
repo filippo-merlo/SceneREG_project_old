54c96841e2ff638e3a9d74c17aacb35c10030183
Filippo <filippo99merlo@gmail.com>
2024-06-17 15:25:55 +0200

    refactor: Improve prompt generation in compute_perplexity_similarity.py

--- code/dataset/compute_scene_obj_similarity /compute_perplexity_similarity.py
+++ code/dataset/compute_scene_obj_similarity /compute_perplexity_similarity.py
@@ -106,8 +106,8 @@ for scene_name in scenes_categories[:1]:
                 article = 'an'
             else:
                 article = 'a'
-            prompt = f"Complete the sentence with the name of the appropriate object:"
-            option = f"In the {scene_name.replace('_',' ')} there is " + article + single_candidate + "."
+            prompt = f"Complete the following sentence with the name of the appropriate object: In the {scene_name.replace('_',' ')} there is " + article
+            option = single_candidate + "."
             single_candidate_list_scores.append(get_perplexity(prompt, option, model=model, tokenizer=tokenizer))
         candidate_scores.append((candidate, np.mean([score for score in single_candidate_list_scores])))
 
