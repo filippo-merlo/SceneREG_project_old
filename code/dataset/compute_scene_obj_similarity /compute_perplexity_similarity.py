e6b2fe2363a8679fb8ca113c8cd89735eb9bc844
Filippo <filippo99merlo@gmail.com>
2024-06-18 13:26:38 +0200

    refactor: Update temperature and prompt generation in collect_llms_norms.py and compute_perplexity_similarity.py

--- code/dataset/compute_scene_obj_similarity /compute_perplexity_similarity.py
+++ code/dataset/compute_scene_obj_similarity /compute_perplexity_similarity.py
@@ -94,7 +94,7 @@ from datasets import load_dataset
 ade_hf_data = load_dataset("scene_parse_150", cache_dir='/mnt/cimec-storage6/shared/hf_datasets')
 scenes_categories = ade_hf_data['train'].features['scene_category'].names
 
-for scene_name in ['bathroom']:#scenes_categories[:1]:
+for scene_name in scenes_categories[:3]:
     candidate_scores = []
     for candidate in candidates:
         single_candidate_list = candidate.split(', ')
@@ -104,13 +104,13 @@ for scene_name in ['bathroom']:#scenes_categories[:1]:
                 article = 'an'
             else:
                 article = 'a'
-            prompt = f"Complete the following sentence with the name of the appropriate object: In the {scene_name.replace('_',' ')} there is " + article
-            option = single_candidate + "."
+            prompt = f"In the {scene_name.replace('_',' ')} there is " + article
+            option = single_candidate
             single_candidate_list_scores.append(get_perplexity(prompt, option, model=model, tokenizer=tokenizer))
         candidate_scores.append((candidate, np.sum([score for score in single_candidate_list_scores])))
 
     sorted_candidate_score = sorted(candidate_scores, key=lambda x: x[1], reverse=True)
     print(f"Scene: {prompt}")
-    for i, (option, score) in enumerate(sorted_candidate_score[:100]):
+    for i, (option, score) in enumerate(sorted_candidate_score[:20]):
         print(f"{i+1}. {option}: {score:.2f}")
     print("\n")
