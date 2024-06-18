2e787cceed074679cdefc7623c28674514cfabb8
Filippo <filippo99merlo@gmail.com>
2024-06-18 15:53:19 +0200

    refactor: Update compute_scene_obj_similarity/compute_log_probs.py to use log softmax for computing log probabilities

--- code/dataset/compute_scene_obj_similarity /compute_log_probs.py
+++ code/dataset/compute_scene_obj_similarity /compute_log_probs.py
@@ -24,7 +24,7 @@ def get_log_probs(prompt, option, model=None, tokenizer=None, log=False):
 
         # Get the model output (logits) and compute log probabilities
         outputs = model(input_ids=current_input_ids)
-        logprobs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        logprobs = torch.nn.functional.log_softmax(outputs.logits, dim=-1)
 
         # Store the logits for the current step
         next_target_token_id = target_ids[:, i].item()
