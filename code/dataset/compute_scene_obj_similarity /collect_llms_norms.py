2b3c0de1b07af7a74bb8c65493d53c5270831a5f
Filippo <filippo99merlo@gmail.com>
2024-06-18 14:02:42 +0200

    refactor: Update collect_llms_norms.py to iterate over 1000 candidates instead of 300

--- code/dataset/compute_scene_obj_similarity /collect_llms_norms.py
+++ code/dataset/compute_scene_obj_similarity /collect_llms_norms.py
@@ -19,7 +19,6 @@ model = AutoModelForCausalLM.from_pretrained(
     token=ACCESS_TOKEN
 )
 
-#%%
 import pickle as pkl
 # get objects and scenes names 
 
@@ -30,37 +29,43 @@ with open('{}/{}'.format(DATASET_PATH, index_file), 'rb') as f:
     index_ade20k = pkl.load(f)
 candidates = index_ade20k['objectnames']
 
-def collect_norm(model, tokenizer, prompt):
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
-    outputs = model(input_ids=input_ids)
-    return outputs
+# Load scene categories from ADE20K hf
+from datasets import load_dataset
+ade_hf_data = load_dataset("scene_parse_150", cache_dir='/mnt/cimec-storage6/shared/hf_datasets')
+scenes_categories = ade_hf_data['train'].features['scene_category'].names
 
-prompt = 'How likely is in a scale of 1 to 100 that the object: [OBJECT] is present in the scene: [SCENE]?'
+for scene_name in scenes_categories[:1]:
+    candidate_scores = []
+    for candidate in candidates[:1]:
+        candidate_list = candidate.split(', ')
+        candidate_scores = []
+        for single_candidate in candidate_list:
+            prompt = "On a scale from 1 (not related at all) to 7 (very related), how related is the object '{word}' with the place '{scene}'?".format(word=single_candidate, scene=scene_name.replace("_", " "))
+            messages = [
+                {"role": "system", "content": "You are a helpful assistant. Your job is to rate how much each object is related to a specific place."},
+                {"role": "user", "content": "On a scale from 1 (not related at all) to 7 (very related), how related is the object '{word}' with the place '{scene}'?"}
+            ]
 
+            input_ids = tokenizer.apply_chat_template(
+                messages,
+                add_generation_prompt=True,
+                return_tensors="pt"
+            ).to(model.device)
 
-messages = [
-    {"role": "system", "content": "You are an expert that knows everything about scenes and objects and responds only with numbers!"},
-    {"role": "user", "content": "How likely is in a scale of 1 to 100 that the object: cup is present in the scene: bathroom?"},
-]
+            terminators = [
+                tokenizer.eos_token_id,
+                tokenizer.convert_tokens_to_ids("<|eot_id|>")
+            ]
 
-input_ids = tokenizer.apply_chat_template(
-    messages,
-    add_generation_prompt=True,
-    return_tensors="pt"
-).to(model.device)
-
-terminators = [
-    tokenizer.eos_token_id,
-    tokenizer.convert_tokens_to_ids("<|eot_id|>")
-]
-
-outputs = model.generate(
-    input_ids,
-    max_new_tokens=256,
-    eos_token_id=terminators,
-    do_sample=True,
-    temperature=0.1,
-    top_p=0.9,
-)
-response = outputs[0][input_ids.shape[-1]:]
-print(tokenizer.decode(response, skip_special_tokens=True))
+            outputs = model.generate(
+                input_ids,
+                max_new_tokens=10,
+                eos_token_id=terminators,
+                do_sample=True,
+                temperature=0.1,
+                top_p=0.9,
+            )
+            response = outputs[0][input_ids.shape[-1]:]
+            decoded_response = tokenizer.decode(response, skip_special_tokens=True)
+            candidate_scores.append(decoded_response)
+    print(candidate_scores)
