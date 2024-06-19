59367293e6c59be8709c74040d4d526295d6ca26
Filippo <filippo99merlo@gmail.com>
2024-06-19 15:18:56 +0200

    refactor: Update compute_scene_obj_similarity/collect_llms_norms.py to improve candidate scoring and prompt consistency

--- code/dataset/compute_scene_obj_similarity /collect_llms_norms.py
+++ code/dataset/compute_scene_obj_similarity /collect_llms_norms.py
@@ -52,8 +52,10 @@ for scene_name in scenes_categories[:10]:
             input_ids = tokenizer.apply_chat_template(
                 messages,
                 add_generation_prompt=True,
-                return_tensors="pt"
+                return_tensors="pt",
             ).to(model.device)
+
+            attention_mask = attention_mask = input_ids["attention_mask"]
 
             terminators = [
                 tokenizer.eos_token_id,
@@ -62,6 +64,7 @@ for scene_name in scenes_categories[:10]:
 
             outputs = model.generate(
                 input_ids,
+                attention_mask=attention_mask,
                 max_new_tokens=10,
                 eos_token_id=terminators,
                 do_sample=True,
@@ -71,7 +74,7 @@ for scene_name in scenes_categories[:10]:
 
             response = outputs[0][input_ids.shape[-1]:]
             decoded_response = tokenizer.decode(response, skip_special_tokens=True)
-            
+
             # Add answer for the list of candidates only if a YES was not obtained yet
             if decoded_response in ['YES', 'NO']:
                 try:
